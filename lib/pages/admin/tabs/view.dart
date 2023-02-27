ba70ee8c23191cd6d2b09e5f337ea7e2c9a479aa
Oguz Vuruskaner <ovuruska@outlook.com>
2023-02-27 17:16:37 +0300

    Admin page is finalized.

--- lib/pages/admin/tabs/view.dart
+++ lib/pages/admin/tabs/view.dart
@@ -11,47 +11,17 @@ class AdminTabsView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
-      length: 3, // length of tabs
-      initialIndex: 0,
-      child: Scaffold(
-        backgroundColor: FlutterFlowTheme.of(context).primaryBackground,
-        body: SafeArea(
-            child: Column(
-          mainAxisSize: MainAxisSize.max,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Header(),
-            TabBar(tabs: [
-              Tab(
-                  child: Text(
-                'Branches',
-                style:
-                    TextStyle(color: FlutterFlowTheme.of(context).primaryText),
-              )),
-              Tab(
-                  child: Text(
-                'Employees',
-                style:
-                    TextStyle(color: FlutterFlowTheme.of(context).primaryText),
-              )),
-              Tab(
-                  child: Text(
-                'Services',
-                style:
-                    TextStyle(color: FlutterFlowTheme.of(context).primaryText),
-              )),
-            ]),
-            Expanded(
-                child: TabBarView(
-                    physics: NeverScrollableScrollPhysics(),
-                    children: [
-                  AdminBranchesTab(),
-                  AdminEmployeeTabView(),
-                  AdminServicesTabView(),
-                ])),
-          ],
-        )),
-      ),
-    );
+        length: 3, // length of tabs
+        initialIndex: 0,
+        child: Scaffold(
+            backgroundColor: FlutterFlowTheme.of(context).primaryBackground,
+            body: SafeArea(
+                child: Column(
+                    mainAxisSize: MainAxisSize.max,
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: <Widget>[
+                  Header(),
+                  Expanded(child: AdminBranchesTab())
+                ]))));
   }
 }
